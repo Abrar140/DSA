3574b55f725c2a6113cc2d379abe236d79ca960e
Abrar140 <muhamadabraramjad@gmail.com>
2025-02-13 10:09:34 +0500

    CIrcular L L

---
+++ 11-CircularSingularLinkList.py
@@ -0,0 +1,84 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.head.next = self.head
+        else:
+            temp = self.head
+            while temp.next != self.head:
+                temp = temp.next
+            temp.next = new_node
+            new_node.next = self.head
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.head.next = self.head
+        else:
+            temp = self.head
+            while temp.next != self.head:
+                temp = temp.next
+            temp.next = new_node
+            new_node.next = self.head
+            self.head = new_node
+
+    def delete_at_beginning(self):
+        if not self.head:
+            return
+        if self.head.next == self.head:
+            self.head = None
+        else:
+            temp = self.head
+            while temp.next != self.head:
+                temp = temp.next
+            temp.next = self.head.next
+            self.head = self.head.next
+
+    def delete_at_end(self):
+        if not self.head:
+            return
+        if self.head.next == self.head:
+            self.head = None
+        else:
+            temp = self.head
+            prev = None
+            while temp.next != self.head:
+                prev = temp
+                temp = temp.next
+            prev.next = self.head
+
+    def display(self):
+        if not self.head:
+            print("List is empty")
+            return
+        temp = self.head
+        while True:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+            if temp == self.head:
+                break
+        print()
+
+# Example usage
+cll = CircularLinkedList()
+cll.insert_at_end(10)
+cll.insert_at_end(20)
+cll.insert_at_end(30)
+cll.insert_at_beginning(5)
+cll.display()  # Output: 5 -> 10 -> 20 -> 30 ->
+
+cll.delete_at_beginning()
+cll.display()  # Output: 10 -> 20 -> 30 ->
+
+cll.delete_at_end()
+cll.display()  # Output: 10 -> 20 ->
